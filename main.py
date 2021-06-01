b9855f715bbe3d83bd58046a98e94da7fb09f06a
Gayathri Ramachandran <gayathriramachandran@Gayathris-MacBook-Air.local>
2021-06-01 22:24:16 +0530

    after first error

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from flask import Flask, request, jsonify, render_template, abort
 
 API_KEY_OPENWEATHER = '78b0f2d6c78691b49ebee3c031b2f52b'
 
-# personal_details = [{'info' : {'name' : 'John',
-#     'position' : 'Developer',
-#     'company' : 'Google',
-#     'languages' : ['Python', 'HTML', 'CSS', 'Java']
-# }}]
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -31,23 +25,5 @@ def show_weather():
         # return f'{temperature} {climate}'
     abort(404)
 
-# @anything.route('/contact')
-# def this_is_for_contact():
-#     return '<h1>This is the contact page </h1>'
-
-# @anything.route('/member-name/<int:id>')
-# def capture_name(id):
-#     return f'<h1> The id given was {id}. </h1>'
-
-# @anything.route('/query')
-# def capture_query_strings():
-#     name = request.args.get('name')
-#     com = request.args.get('company')
-#     return f'<h1> The name is {name} and company is {com} </h1>'
-
-# @anything.route('/convert-json')
-# def this_is_to_convert_json():
-#     # return personal_details
-#     return jsonify(personal_details)
-
-app.run(host='127.0.0.1', port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
